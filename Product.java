080abbd5c61cffa9ada345c7ac820a725d7b424d
wasf4294 <126531134+wasf4294@users.noreply.github.com>
2023-04-03 22:44:54 +0900

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,50 +1,68 @@
-public class Product {
+
+class Product {
     int price;
     int bonusPoint;
-
     Product(int price){
         this.price = price;
-        bonusPoint = (int)(price/10.0);
+        bonusPoint =(int)(price/10.0);
     }
+    Product(){}    //기본 생성자(없으면 안됌)
 }
 
-class Tv extends Product    {
-    Tv()    {
-        super(100);
-    }
-    public String toString(){
-        return "Tv";
-    }
+class Tv extends Product{
+    Tv(){super(100);}
+    public String toString(){return "Tv";}
 }
 
-class Computer extends Product  {
-    Computer(){super(200);}
+class Computer extends Product{
+    Computer(){super(100);}
     public String toString(){return "Computer";}
+}
+
+class Audio extends Product{
+    Audio(){super(100);}
+    public String toString(){return "Audio";}
 }
 
 class Buyer {
     int money = 1000;
     int bonusPoint = 0;
+    Product[] item = new Product[10];       //0~9까지 10개의 배열 생성
+    int i = 0;
 
     void buy(Product p){
-        if (money < p.price)    {
-            System.out.println("너 돈없어서 못사 이거");
+        if (money < p.price){
+            System.out.println("비싸서 못사");
             return;
         }
+
         money -= p.price;
         bonusPoint += p.bonusPoint;
+        item[i++]=p;
         System.out.println(p + "을/를 구매하셨습니다.");
     }
-}
 
-class PolyArgumentTest  {
+    void summary(){
+        int sum = 0;
+        String itemList ="";
+
+        for(int i=0; i<item.length; i++){
+            if (item[i]==null)break;
+            sum += item[i].price;
+            itemList += item[i] + ", ";
+        }
+        System.out.println("총" +sum+ "만원");
+        System.out.println(itemList);
+    }
+}
+class PolyArgumentTest2{
     public static void main(String[] args) {
         Buyer b = new Buyer();
 
         b.buy(new Tv());
         b.buy(new Computer());
+        b.buy(new Audio());
 
-        System.out.println("남은돈" + b.money);
-        System.out.println("보너스" + b.bonusPoint);
+        b.summary();
     }
 }
